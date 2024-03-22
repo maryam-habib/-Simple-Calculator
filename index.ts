c01bca4ce62b426c9ecd40e14dcc87cdf02641a1
Your Name <you@example.com>
2024-03-22 10:07:22 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter  first number", type: "number", name: "firstNumber" },
   { message: "Enter second number", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to perform operation",
@@ -24,6 +24,6 @@ if (answer.operator === "Addition") {
     console.log(answer.firstNumber / answer.secondNumber);
 }
  else {
-    console.log("Please select valid operator");
+    console.log("The End");
 }
 
